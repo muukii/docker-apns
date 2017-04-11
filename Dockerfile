ed238f87cd37c18658e041b4c813e43d0efee269
muukii <m@muukii.me>
2017-04-11 10:16:50 +0900

    :evergreen_tree: Update

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ MAINTAINER Muukii <m@muukii.me>
 RUN apt-get -q update
 RUN apt-get -q install -y jq
 RUN gem install houston
+COPY ./script.sh /usr/local/bin/script.sh
 
-ENTRYPOINT ["apn"]
+ENTRYPOINT ["sh /usr/local/bin/script.sh"]
 
--- script.sh
+++ script.sh
@@ -1,4 +1,17 @@
 #! bin/bash
 
-#TODO
+TOKEN=$1
+JSON=$2
+
+if [ -z $TOKEN ]; then
+    echo error: invalid TOKEN
+fi
+
+if [ -z $JSON ]; then
+    echo error: invalid JSON
+fi
+
+_JSON=`echo $JSON | tr '\n' ' ' | sed -e 's# ##g'`
+
+apn push "${TOKEN}" -c "/usr/local/private_key/push.pem" -P ${_JSON}
 
