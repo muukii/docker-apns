841fe4dc8857f4e220afc6c43a8628175d291674
muukii <m@muukii.me>
2017-04-11 11:09:14 +0900

    :evergreen_tree: Update

--- script.sh
+++ script.sh
@@ -1,17 +1,21 @@
 #! bin/bash
 
 TOKEN=$1
-JSON=$2
+JSON=$(cat $2)
 
-if [ -z $TOKEN ]; then
+echo $JSON
+
+if [ -z "$TOKEN" ]; then
     echo error: invalid TOKEN
 fi
 
-if [ -z $JSON ]; then
+if [ -z "$JSON" ]; then
     echo error: invalid JSON
 fi
 
 _JSON=`echo $JSON | tr '\n' ' ' | sed -e 's# ##g'`
+
+echo $_JSON
 
 apn push "${TOKEN}" -c "/usr/local/private_key/push.pem" -P ${_JSON}
 
